b66b7d53116e9dbcdf37af9f6793d44ff1443f9e
edwardfrary <edwardfrary@gmail.com>
2021-06-16 13:59:30 -0400

    update

--- _tests_/manager.test.js
+++ _tests_/manager.test.js
@@ -3,6 +3,7 @@ const Manager = require('../lib/Manager.js');
 
 test('Enter and store the managers name, employee ID, email and office number', () => {
     const manager = new Manager('Dave', '08830', 'daveonthetrain@scooter.net', 13);
+    expect(manager.type).toBe('Manager');
     expect(manager.name).toBe('Dave');
     expect(manager.empID).toBe('08830');
     expect(manager.email).toBe('daveonthetrain@scooter.net');
